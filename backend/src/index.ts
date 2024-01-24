0788b0649fb5b989da726cc35e874253a7ed192e
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2024-01-24 19:15:16 -0400

    enabling cors controls changed server port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express';
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+//cors
+const options = {
+  origin: 'http://localhost:3000',
+}
+app.use(cors(options))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({hello: "world"})
+  res.json({ hello: "world" })
 });
 
 
