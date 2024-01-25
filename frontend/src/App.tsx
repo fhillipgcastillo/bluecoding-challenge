d16bb6774120e0acd563f69513910c81f8f2d9ad
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2024-01-25 16:44:10 -0400

    Added simple create link shortener frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+interface LinkResponse {
+  _id: string;
+  shortLink: string;
+}
 function App() {
+  const linkRef = useRef<HTMLInputElement>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [shortenUrl, setShortenUrl] = useState<LinkResponse | null>(null)
+
+  useLayoutEffect(() => {
+
+
+
+  }, [])
+  const shorten = async () => {
+    const url = linkRef.current?.value.trim();
+
+    await fetch("http://localhost:8080/shortener",
+      {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({ url }), // body data type must match "Content-Type" header
+      }
+    )
+      .then(r => r.json())
+      .then((data) => {
+        setShortenUrl(data);
+      })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className='short-link-wrapper'
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
         >
-          Learn React
-        </a>
+          <label htmlFor="link">Short Link</label>
+          <input id='link' placeholder='https://google.com' ref={linkRef} />
+          <button onClick={shorten}>Shorten</button>
+        </div>
+        {shortenUrl && <div>
+          <label>id {shortenUrl?._id}</label>
+          <label>id <a href={shortenUrl.shortLink}>{shortenUrl.shortLink}</a></label>
+        </div>}
       </header>
     </div>
   );
